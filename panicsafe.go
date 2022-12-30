6cfde906d6ba36c3562bc8cb7810b83686270385
Arnaud Briche <briche.arnaud@gmail.com>
2022-12-30 16:19:40 +0100

    chore: add new RecoverXXXX functions

--- panicsafe.go
+++ panicsafe.go
@@ -27,6 +27,54 @@ func Func(f func() error) func() error {
 	}
 }
 
+func Recover(f func() error) error {
+	return RecoverWithHandler(f, nil)
+}
+
+func RecoverWithHandler(f func() error, h func(error)) (retErr error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err, ok := r.(error)
+
+			if ok {
+				retErr = err
+			} else {
+				retErr = fmt.Errorf("%v", r)
+			}
+
+			if h != nil {
+				h(retErr)
+			}
+		}
+	}()
+
+	return f()
+}
+
+func Recover2[T any](f func() (T, error)) (T, error) {
+	return Recover2WithHandler(f, nil)
+}
+
+func Recover2WithHandler[T any](f func() (T, error), h func(error)) (ret T, retErr error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err, ok := r.(error)
+
+			if ok {
+				retErr = err
+			} else {
+				retErr = fmt.Errorf("%v", r)
+			}
+
+			if h != nil {
+				h(retErr)
+			}
+		}
+	}()
+
+	return f()
+}
+
 func LogErrorFunc(logger logr.Logger, msg string, f func() error) func() {
 	return FuncWithErrorLog(logger, msg, f)
 }
